3de0547bfb01a6cc9b2773555d668834ec4f4423
farfarfun <farfarfun@qq.com>
2026-08-30 10:14:35 +0800

    test: add minimal smoke test (tests/test_smoke.py)
    Refs farfarfun/todo-list#304

---
+++ tests/test_smoke.py
@@ -0,0 +1,10 @@
+import funmovie
+import funmovie.utils
+
+
+def test_import_funmovie():
+    assert funmovie is not None
+
+
+def test_import_funmovie_utils():
+    assert funmovie.utils is not None
